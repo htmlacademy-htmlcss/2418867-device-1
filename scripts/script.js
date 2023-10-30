af5eb912c58bc2f9702f65146ea339ab4c4f7cbb
Tiizara <135403209+Tiizara@users.noreply.github.com>
2023-10-30 12:57:49 +0500

    all

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,14 @@
 const deliveryLink = document.querySelector('.delivery-rare-link');
 const modal = document.querySelector('.modal');
+const modalCloseButton = document.querySelector('.modal-close-button');
 
 deliveryLink.addEventListener('click', (evt) => {
     evt.preventDefault();
     modal.classList.remove('modal--close');
-})
+});
+
+
+modalCloseButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    modal.classList.add('modal--close');
+});
