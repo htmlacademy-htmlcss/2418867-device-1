86858bf86c21acff60a6ea55fe28f210e7130eee
Tiizara <135403209+Tiizara@users.noreply.github.com>
2023-11-03 09:42:18 +0500

    all

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
 const deliveryLink = document.querySelector('.delivery-rare-link');
 const modal = document.querySelector('.modal');
 const modalCloseButton = document.querySelector('.modal-close-button');
+const deliveryList = document.querySelector('.delivery-list');
+const deliveryItems = document.querySelectorAll('.delivery-button');
+const deliveryScreens = document.querySelectorAll('.delivery-screen');
 
 deliveryLink.addEventListener('click', (evt) => {
     evt.preventDefault();
@@ -12,3 +15,14 @@ modalCloseButton.addEventListener('click', (evt) => {
     evt.preventDefault();
     modal.classList.add('modal--close');
 });
+
+if (deliveryList) {
+    deliveryList.addEventListener('click', (evt) => {
+       const indexClickedButton =  Array.from(deliveryItems).indexOf(evt.target);
+       document.querySelector('.delivery-button-current').classList.remove('delivery-button-current');
+       evt.target.closest('.delivery-item').classList.add('delivery-button-current');
+
+       document.querySelector('.delivery-screen-active').classList.remove('delivery-screen-active');
+       Array.from(deliveryScreens)[indexClickedButton].classList.add('delivery-screen-active');
+    });
+}
